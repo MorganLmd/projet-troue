f637525e694718e84f713707f337634fd9e2eee3
Morgan Lombard <morgan.lombard@protonmail.com>
2021-02-10 19:19:38 +0100

    ajout sécu sur les routes api user

--- src/main/java/com/m1iii/cybersecu/projettroue/controllers/UserController.java
+++ src/main/java/com/m1iii/cybersecu/projettroue/controllers/UserController.java
@@ -2,6 +2,7 @@ package com.m1iii.cybersecu.projettroue.controllers;
 
 import com.m1iii.cybersecu.projettroue.model.User;
 import com.m1iii.cybersecu.projettroue.repositories.UserRepository;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import javax.transaction.Transactional;
@@ -25,31 +26,37 @@ public class UserController {
     }
 
     @GetMapping
+    @PreAuthorize("hasAnyRole('ROLE_ADMIN')")
     public Iterable<User> findAll() {
         return this.userRepository.findAll();
     }
 
     @GetMapping(path = "/{userId}")
+    @PreAuthorize("hasAnyRole('ROLE_ADMIN')")
     public Optional<User> findById(@PathVariable("userId") Long userId) {
         return this.userRepository.findById(userId);
     }
 
     @GetMapping(path = "/email/{userEmail}")
+    @PreAuthorize("hasAnyRole('ROLE_ADMIN')")
     public Optional<User> findByEmail(@PathVariable("userEmail") String userEmail) {
         return this.userRepository.findUserByEmail(userEmail);
     }
 
     @PostMapping("/save")
+    @PreAuthorize("hasAnyRole('ROLE_ADMIN')")
     public User save(@RequestBody User user) {
         return this.userRepository.save(user);
     }
 
     @DeleteMapping("/{userId}")
+    @PreAuthorize("hasAnyRole('ROLE_ADMIN')")
     public void delete(@PathVariable("userId") Long userId) {
         this.userRepository.deleteUserById(userId);
     }
 
     @PutMapping("/update")
+    @PreAuthorize("hasAnyRole('ROLE_ADMIN')")
     public User update(@RequestBody User user) {
         return this.userRepository.save(user);
     }
